f8eb6c59cb9ceb622964ab591f2de3cc23ada4e7
shj <shj@hybus.net>
2024-11-16 00:55:45 +0900

    [resolve #9] User 이름 LocalStorage에 save/load되도록 적용

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+// Element ==================================================
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+
 const greeting = document.querySelector("#greeting");
+// Element ==================================================
 
+
+
+// DEFINE ==================================================
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName"
+// DEFINE ==================================================
 
+
+
+// Event Function ==================================================
 // submit Event
-function onSubmit(event){
+function onLoginSubmit(event){
     // JavaScript는 기본적으로 argument(ex. submit주체, submit 시점, ...)를 담아서 함수를 호출    
     // form을 submit하면 브라우저는 페이지를 새로고침 (기본동작)
     // preventDefault() : 브라우저의 기본 동작을 막음 ('첫 번째 argument' 안에 있는 함수)
@@ -15,7 +26,30 @@ function onSubmit(event){
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
     const userName = loginInput.value;
+    localStorage.setItem("userName", userName);
+    paintGreeting(userName);
+}
+// Event Function ==================================================
+
+
+
+// Function ==================================================
+// Greeting 문구 표시
+function paintGreeting(userName){
     greeting.innerText = `Hello ${userName}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-loginForm.addEventListener("submit", onSubmit);
+// Function ==================================================
+
+
+
+// Main ==================================================
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+if(savedUserName == null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    paintGreeting(savedUserName);
+}
+// Main ==================================================
