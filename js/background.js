ee014c231285dcf31d37b3e1425b519a3b4cb08f
Dawn Surplus <>
2024-11-17 21:02:58 +0900

    [resolve #17] 백그라운드 이미지 추가 및 전체 이미지 적용

--- js/background.js
+++ js/background.js
@@ -7,9 +7,10 @@ const bgImage = document.createElement("img");
 
 // DEFINE ==================================================
 const IMAGES_LIST = [
-    "galaxy.jpg",
-    "moon.jpg",
-    "sky_island.jpg"
+    //"galaxy.jpg",
+    //"moon.jpg",
+    //"sky_island.jpg",
+    "cloud_ring.jpg",
 ]
 // DEFINE ==================================================
 
@@ -18,6 +19,12 @@ const IMAGES_LIST = [
 // Main ==================================================
 const chosenImage = IMAGES_LIST[Math.floor(Math.random()*IMAGES_LIST.length)];
 
-bgImage.src = `img/${chosenImage}`
-document.body.appendChild(bgImage);
+//bgImage.src = `img/${chosenImage}`
+//document.body.appendChild(bgImage);
+// body 태그의 스타일을 수정하여 배경 이미지 적용
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center center';
+document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.backgroundAttachment = 'fixed';
 // Main ==================================================
